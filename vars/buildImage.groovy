d2d00f51e4560ceea61dffcbe628ea065b6daf98
Janet Cruz <janetcruzangel@gmail.com>
2026-06-16 15:29:01 -0400

    first commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,12 @@
+#!/user/bin/env groovy
+def call(String imageName) {
+    echo 'building the docker image...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+        sh '''
+            echo "Logging in to Docker Hub with username: $DOCKER_USERNAME"
+            echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+            docker build -t janetcruzangel/demo-app:jma-2.0 .
+            docker push janetcruzangel/demo-app:jma-2.0
+        '''
+    }
+}
